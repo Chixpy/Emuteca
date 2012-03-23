c2a3a800c2cd2e0b77fc387a7470bf9d6e010a89
Chixpy <chixpy@gmail.com>
2012-03-23 22:52:21 +0100

    Rellenando un poco el gestor de configuración

--- ObjPas/Lazarus/Source/fConfigManager.pas
+++ ObjPas/Lazarus/Source/fConfigManager.pas
@@ -6,15 +6,29 @@ interface
 
 uses
   Classes, SysUtils, FileUtil, LResources, Forms, Controls, Graphics, Dialogs,
-  StdCtrls, uConfig;
+  StdCtrls, ExtCtrls, ComCtrls, EditBtn, uConfig;
 
 type
 
   { TfrmConfigManager }
 
   TfrmConfigManager = class(TForm)
+    e7zPath: TFileNameEdit;
+    emPlayerPath: TFileNameEdit;
     gbxImageExt: TGroupBox;
+    gbxCompExt: TGroupBox;
+    gbxTextExt: TGroupBox;
+    l7zPath: TLabel;
+    lmPlayerPath: TLabel;
+    mCompExt: TMemo;
+    mTextExt: TMemo;
     mImageExt: TMemo;
+    p7zPath: TPanel;
+    pmPlayerPath: TPanel;
+    pBottom: TPanel;
+    pcConfig: TPageControl;
+    pagExtensions: TTabSheet;
+    pagPaths: TTabSheet;
 
   private
     { private declarations }
@@ -39,8 +53,18 @@ begin
   FConfig := AValue;
 
   if AValue = nil then Exit;
+
+  // Paths
+  e7zPath.Text:= AValue.z7Subfolder + AValue.z7CMExecutable;
+  emPlayerPath.Text:=AValue.mPlayerSubfolder + AValue.mPlayerExecutable;
+
+  // Extensions
   mImageExt.Clear;
   mImageExt.Lines.AddStrings(AValue.ImageExtensions);
+  mTextExt.Clear;
+  mTextExt.Lines.AddStrings(AValue.TextExtensions);
+  mCompExt.Clear;
+  mCompExt.Lines.AddStrings(AValue.CompressedExtensions);
 
 end;
 
